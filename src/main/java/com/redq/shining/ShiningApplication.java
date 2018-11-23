ee307edff3ad17262712dd4e812ade75526f4ee7
ahlon <ahlon2002@gmail.com>
2018-11-23 20:15:36 +0800

    init projects

---
+++ src/main/java/com/redq/shining/ShiningApplication.java
@@ -0,0 +1,12 @@
+package com.redq.shining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiningApplication.class, args);
+    }
+}
