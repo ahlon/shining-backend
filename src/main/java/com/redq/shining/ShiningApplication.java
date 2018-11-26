392b7a50d72d668d148517848508073aec0fc038
ahlon <ahlon2002@gmail.com>
2018-11-26 21:52:00 +0800

    using mapper instead of using mapper scan

--- src/main/java/com/redq/shining/ShiningApplication.java
+++ src/main/java/com/redq/shining/ShiningApplication.java
@@ -3,7 +3,6 @@ package com.redq.shining;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@tk.mybatis.spring.annotation.MapperScan(basePackages = "com.redq.shining.mapper")
 @SpringBootApplication
 public class ShiningApplication {
 
