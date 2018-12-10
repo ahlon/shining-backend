6aa6237e0536f35523c4a7a238efe4b565c8bf03
ahlon <ahlon2002@gmail.com>
2018-12-10 23:24:01 +0800

    add common data output schema

---
+++ src/main/java/com/redq/shining/common/ResponseResult.java
@@ -0,0 +1,45 @@
+package com.redq.shining.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author ahlon
+ */
+@Data
+public class ResponseResult<T> {
+
+    private boolean success;
+    private String message;
+    private Date timestamp;
+    private T data;
+
+    public ResponseResult(boolean success, T data) {
+        this.success = success;
+        this.timestamp = new Date();
+        this.data = data;
+    }
+
+    public ResponseResult(boolean success, String message, T data) {
+        this.success = success;
+        this.timestamp = new Date();
+        this.message = message;
+        this.data = data;
+    }
+
+    public static ResponseResult success(Object data) {
+        return new ResponseResult(true, "", data);
+    }
+
+    public static ResponseResult success(Object data, String message) {
+        return new ResponseResult(true, message, data);
+    }
+
+    public static ResponseResult fail(Object data, String message) {
+        return new ResponseResult(false, message, data);
+    }
+
+
+}
