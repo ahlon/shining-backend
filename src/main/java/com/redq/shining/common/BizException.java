bba9c2c9988f0737b4f249086de9c0b1241d5e8a
ahlon <ahlon2002@gmail.com>
2018-12-14 16:30:26 +0800

    add base biz exception

---
+++ src/main/java/com/redq/shining/common/BizException.java
@@ -0,0 +1,18 @@
+/**
+ * Ouer.com Inc.
+ * Copyright (c) 2014-2015 All Rights Reserved.
+ */
+package com.redq.shining.common;
+
+/**
+ * 
+ * @author tonghu
+ */
+public class BizException extends RuntimeException {
+
+    private static final long serialVersionUID = 1776437705263495268L;
+
+    public BizException(String message) {
+        super(message);
+    }
+}
