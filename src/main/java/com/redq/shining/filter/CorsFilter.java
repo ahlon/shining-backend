8a3da6040c87eb73ec72486398050845883f73ea
ahlon <ahlon2002@gmail.com>
2018-12-14 16:32:06 +0800

    add cors filter

---
+++ src/main/java/com/redq/shining/filter/CorsFilter.java
@@ -0,0 +1,35 @@
+package com.redq.shining.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CorsFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+		response.setHeader("Access-Control-Allow-Origin", request.getHeader("origin"));
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Cache-Control,Pragma,Content-Type,Token");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Content-Type", "application/x-www-form-urlencoded");
+		chain.doFilter(req, res);
+	}
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+}
